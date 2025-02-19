218f99c7974585b39b7cb821335f8b25e28eabae
Jakub Labath <jakub@labath.ca>
2025-02-19 12:34:34 +0000

    add the ability to use query tokens

--- main.go
+++ main.go
@@ -83,6 +83,11 @@ func (r *Row) MarshalJSON() ([]byte, error) {
 	return json.Marshal(r.columns)
 }
 
+type QueryTokenResp struct {
+	Rows  []Row  `json:"rows"`
+	Token string `json:"token"`
+}
+
 func query(client *bigquery.Client) pod.Handler {
 
 	return func(ctx context.Context, args []json.RawMessage) (json.RawMessage, error) {
@@ -120,6 +125,52 @@ func query(client *bigquery.Client) pod.Handler {
 	}
 }
 
+func queryToken(client *bigquery.Client) pod.Handler {
+
+	return func(ctx context.Context, args []json.RawMessage) (json.RawMessage, error) {
+		var (
+			sql      string
+			token    string
+			pageSize int
+			results  []Row
+		)
+
+		if err := pod.DecodeArgs(args, &sql, &token, &pageSize); err != nil {
+			return nil, err
+		}
+
+		// Run the query
+		q := client.Query(sql)
+		it, err := q.Read(ctx)
+		if err != nil {
+			log.Fatalf("Failed to execute query: %v", err)
+		}
+		if token != "" {
+			it.PageInfo().Token = token
+		}
+		it.PageInfo().MaxSize = pageSize
+
+		// Iterate through the results
+		for i := 0; i < pageSize; i = i + 1 {
+			var row Row
+			err := it.Next(&row)
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error iterating through results: %v", err)
+			}
+
+			results = append(results, row)
+		}
+		resp := QueryTokenResp{
+			Token: it.PageInfo().Token,
+			Rows:  results,
+		}
+		return json.Marshal(resp)
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("Missing a filepath argument for socket to listen on\n")
@@ -147,10 +198,13 @@ func main() {
 		Format: "json",
 		Namespaces: []pod.Namespace{pod.Namespace{
 			Name: "netpod.jlabath.bigquery",
-			Vars: []pod.Var{pod.Var{
-
-				Name:    "query",
-				Handler: query(client)},
+			Vars: []pod.Var{
+				pod.Var{
+					Name:    "query",
+					Handler: query(client)},
+				pod.Var{
+					Name:    "query-token",
+					Handler: queryToken(client)},
 			}},
 		}}
 
